bb05027f137f3779d6ac61df7ad0b18ad57d77cd
benja <nadninfianfonfanofan@gmail.com>
2026-09-20 20:45:29 -0300

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -126,7 +126,6 @@ function crearFlores(x, y) {
 
 }
 function abrirCarta() {
-
     const carta = document.getElementById("cartaSecreta");
 
     carta.classList.add("activa");
@@ -137,13 +136,10 @@ function abrirCarta() {
         window.innerWidth / 2,
         window.innerHeight / 2
     );
-
 }
 
 function cerrarCarta() {
-
     const carta = document.getElementById("cartaSecreta");
 
     carta.classList.remove("activa");
-
 }
